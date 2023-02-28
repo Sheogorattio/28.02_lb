26832cdf3f1981b1bb53984e7fcb759cb0b96af7
Sheogorattio <no.passion.there.is.serenity@gmail.com>
2023-02-28 19:37:49 +0200

    Add project files.

---
+++ 28.02_lb/WordCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._02_lb
+{
+    public class WordCounter
+    {
+        string[] text;
+        SortedList<string, int> wordCounter;
+        public WordCounter(string text)
+        {
+            this.text = text.Split();
+        }
+        public void Count()
+        {
+            foreach (string word in text) {
+                for(int i = 0; i < wordCounter.Count; i++)
+                {
+                    if (wordCounter.ContainsKey(word))
+                    {
+                        wordCounter[word]++;
+                    }
+                    else
+                    {
+                        wordCounter.Add(word, 1);
+                    }
+                }
+            }
+        }
+        public void Result()
+        {
+            foreach(var i  in wordCounter)
+            {
+                Console.WriteLine(i.Key + "\t"+ i.Value);
+            }
+        }
+    }
+}
